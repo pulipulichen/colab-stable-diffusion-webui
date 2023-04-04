176cd80768a601e9457177208b0592739b0f6472
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-05 00:31:04 +0800

    deb

--- script.sh
+++ script.sh
@@ -6,16 +6,6 @@ if test -f "$FILE"; then
 else
     echo "$FILE not exists."
 
-    apt -y update -qq
-    wget http://launchpadlibrarian.net/367274644/libgoogle-perftools-dev_2.5-2.2ubuntu3_amd64.deb
-    wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/google-perftools_2.5-2.2ubuntu3_all.deb
-    wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libtcmalloc-minimal4_2.5-2.2ubuntu3_amd64.deb
-    wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libgoogle-perftools4_2.5-2.2ubuntu3_amd64.deb
-    apt install -qq libunwind8-dev
-    dpkg -i *.deb
-    env LD_PRELOAD=libtcmalloc.so
-    rm *.deb
-
     apt -y install -qq aria2
     pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 torchtext==0.14.1 torchdata==0.5.1 --extra-index-url https://download.pytorch.org/whl/cu116 -U
     pip install -q xformers==0.0.16 triton==2.0.0 -U
