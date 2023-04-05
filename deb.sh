a9ca3e03257f6a6a6bd797b1d1e612e933da2e0b
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-05 12:01:00 +0800

    touch

--- deb.sh
+++ deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE="/content/drive/My Drive/Colab Notebooks/stable-diffusion-webui-deb/ready.txt"
+FILE="/content/drive/MyDrive/Colab Notebooks/stable-diffusion-webui-deb/ready.txt"
 if test -f "$FILE"; then
     echo "$FILE exists."
 else
@@ -12,6 +12,7 @@ else
     wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libtcmalloc-minimal4_2.5-2.2ubuntu3_amd64.deb
     wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libgoogle-perftools4_2.5-2.2ubuntu3_amd64.deb
 
+    touch "$FILE"
 fi
 
 cd "/content/drive/My Drive/Colab Notebooks/stable-diffusion-webui-deb/"
